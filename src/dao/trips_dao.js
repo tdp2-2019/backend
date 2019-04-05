9553cd4ade5f1c045853c2b1dba6029f21a22812
Gustavo adrian Gimenez <gustavo.gimenez@mercadolibre.com>
2019-04-05 00:04:39 -0300

    Calculate and refresh the current car position in a trip. Calculate the trip price. Trip rating added

--- src/dao/trips_dao.js
+++ src/dao/trips_dao.js
@@ -1,13 +1,21 @@
 Trip = require('../models/trip')
+var polyUtil = require('polyline-encoded');
+const request = require('request');
+
 var trips = [];
 
 var trips_dao = module.exports = {
   
   create : function(body) {
     var id = trips.length + 1;
-    var trip = new Trip(id, body.client, body.driver, body.source, body.destination, body.start_time, body.pets);
-    trips.push(trip);
-    return trip;
+    return this.get_waypoints().then(response => {
+      var trip = new Trip(id, body.client, body.driver, body.source, body.destination, body.start_time, body.pets);
+      trip._points = response.points;
+      trip._duration = response.duration;
+      trip.calculate_price();
+      trips.push(trip);
+      return trip;
+    });
   },
   
   update: function(body) {
@@ -19,13 +27,43 @@ var trips_dao = module.exports = {
     trips.forEach(trip => {
       if (trip.id == id) {
         a_trip = trip;
+        a_trip.current_position = trip.calculate_position();
       }
     });
     return a_trip;
   },
   
   get_all: function() {
-    return trips;
+    var json_trips = []
+    trips.forEach(trip =>{
+      json_trips.push(trip.to_json());
+    });
+    return json_trips;
+  },
+  
+  get_waypoints: function() {
+    return new Promise(resolve => {
+      var origin = '-34.570804' + ',' + '-58.433385';
+      var destination = '-34.551718' + ',' + '-58.451990';
+      var points = [];
+      var response = {
+        points: [],
+        duration: 0
+      }
+      request('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&sensor=false&key=AIzaSyArscBswf0kBzkOnj7LmSktzPSE0zmi830',
+      {json: true}, (err, res, body) => {
+        body.routes[0].legs.forEach(leg => {
+          response.duration += leg.duration.value;
+          list_of_points = leg.steps.forEach(step =>{
+            var decoded_points = polyUtil.decode(step.polyline.points);
+            decoded_points.forEach(point => {
+              response.points.push(point);
+            });
+          });
+        });
+        resolve(response);
+      });
+    });
   },
 
 }
--- src/models/trip.js
+++ src/models/trip.js
@@ -6,10 +6,12 @@ class Trip {
     this._driver = driver;
     this._source = source;
     this._destination = destination;
-    this._start_time = start_time;
+    this._start_time = Date.now();
     this._end_time = end_time;
     this._current_position = source;
     this._pets = pets;
+    this._driver_rating = {}
+    this._client_rating = {}
   }
   
   get id() {
@@ -42,6 +44,54 @@ class Trip {
   
   set driver(driver) {
     this._driver = driver;
+  }
+  
+  set driver_rating(rating) {
+    this._driver_rating.rating = rating;
+  }
+  
+  set client_rating(rating) {
+    this._client_rating.rating = rating;
+  }
+  
+  set client_rating_comment(comment) {
+    this._client_rating.comment = comment;
+  }
+  
+  set driver_rating_comment(comment) {
+    this._driver_rating.comment = comment;
+  }
+  
+  calculate_position() {
+    var now = Date.now()
+    var seconds_from_begginig = (now - this._start_time) / 1000
+    if (seconds_from_begginig <= 0) {
+      return this._points[0];
+    }
+    var seconds_per_interval = this._duration / this._points.length;
+    var index = Math.round(seconds_from_begginig/seconds_per_interval)
+    
+    return this._points[index]
+  }
+  
+  calculate_price() {
+    this._price = this._duration * 20;
+  }
+  
+  to_json() {
+    return {
+      id: this._id,
+      current_position: this._current_position,
+      client: this._client,
+      driver: this._driver,
+      source: this._source,
+      destination: this._destination,
+      duration: this._duration,
+      pets: this._pets,
+      price: this._price,
+      driver_rating: this._driver_rating,
+      client_rating: this._client_rating
+    }
   }
   
 }
