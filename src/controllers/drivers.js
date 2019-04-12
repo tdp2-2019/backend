1af93b76465da2016fc7f577be67fd5416fbe906
JuanPatricioMarshall <juan.pmarshall@hotmail.com>
2019-04-12 17:32:09 -0300

    Pruebo una cosa de headers para solucionar un fetch desde js

--- src/controllers/drivers.js
+++ src/controllers/drivers.js
@@ -2,6 +2,11 @@ var drivers_dao = require('../dao/drivers_dao')
 
 drivers = function(app){
 
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
   app.get('/drivers', (req, res) => {
      res.status(200).send({
       drivers: drivers_dao.get_all()
