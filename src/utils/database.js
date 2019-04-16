fe0b20bb6f5e16305297ad48d59c5f64d28c1116
Gustavo adrian Gimenez <gustavo.gimenez@mercadolibre.com>
2019-04-16 00:34:10 -0300

    Get database config in envs

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,5 +1,5 @@
 var Pool = require('pg-pool')
-/*
+
 configDB = {
   user: process.env.DBUSER,
   password: process.env.DBPASSWORD,
@@ -8,14 +8,15 @@ configDB = {
   database: process.env.DBNAME,
   ssl: true,
 };
-*/
+
+/*
 configDB = {
   user: 'postgres',
   password: 'docker',
   host: '127.0.0.1',
   port: 5432,
   database: 'postgres',
-};
+};*/
 
 
 const pool = new Pool(configDB);
