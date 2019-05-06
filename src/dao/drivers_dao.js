d396f4a5d0c84c5c2c59e42c6552fec35b3b95ca
Gustavo adrian Gimenez <gustavo.gimenez@mercadolibre.com>
2019-05-05 22:55:31 -0300

    Fix query sql string when post driver

--- src/dao/drivers_dao.js
+++ src/dao/drivers_dao.js
@@ -8,8 +8,8 @@ var drivers_dao = module.exports = {
     return new Promise(resolve => {
       connect().
       query('INSERT INTO drivers (name, lastname, telephone, celphone, email, dni, address, brand, model, licensenumber, carcolour,' +
-       ' carlicenseplate, insurancepolicynumber, startworktime, endworktime, currentposition, signup_date, photo_url, car_plate_photo_url, firebase_id' +
-       ' license_photo_url, status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, to_timestamp($14), to_timestamp($15),$16, $17, $18, $19, $20, $21, $22) RETURNING *',
+       ' carlicenseplate, insurancepolicynumber, startworktime, endworktime, currentposition, signup_date, photo_url, car_plate_photo_url, ' +
+       ' license_photo_url, status, firebase_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, to_timestamp($14), to_timestamp($15),$16, $17, $18, $19, $20, $21, $22) RETURNING *',
       [body.name, body.lastname, body.telephone, body.celphone, body.email, body.dni, body.address, body.brand, body.model, body.licensenumber,
         body.carcolour, body.carlicenseplate, body.insurancepolicynumber, body.startworktime, body.endworktime, body.currentPosition,
         body.signup_date, body.photo_url, body.car_plate_photo_url, body.license_photo_url, 'No confirmado', body.firebase_id], (err, res) => {
