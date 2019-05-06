2232f29ecfa21b5854d3521b5260d8faa747aeed
JuanPatricioMarshall <juan.pmarshall@hotmail.com>
2019-05-06 02:47:32 -0300

    Ft cors put (#24)
    * Prueba pra ver si el put funca ahora
    
    * try 2
    
    * Fix
    
    * Fix final(?)
    
    * ahora anda capaz

--- app.js
+++ app.js
@@ -2,15 +2,36 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const cron = require("node-cron");
 var assign_driver_util = require('./src/utils/assign_driver_util')
-
+var cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
 
 app.get('/ping', (req, res) => {
