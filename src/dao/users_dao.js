b1f58add72c83c0a6d5919328c37ac053254f540
Gustavo adrian Gimenez <gustavo.gimenez@mercadolibre.com>
2019-06-04 23:53:11 -0300

    Permit status and comment for users

--- src/dao/users_dao.js
+++ src/dao/users_dao.js
@@ -9,8 +9,8 @@ var users_dao = module.exports = {
   create : function(body) {
     return new Promise(resolve => {
       connect().
-      query('INSERT INTO users (name, dni, lastname, email, telephone, celphone, address, firebase_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
-            [body.name, body.dni, body.lastname, body.email, body.telephone, body.celphone, body.address, body.firebase_id], (err, res) =>{
+      query('INSERT INTO users (name, dni, lastname, email, telephone, celphone, address, firebase_id, status, comment) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
+            [body.name, body.dni, body.lastname, body.email, body.telephone, body.celphone, body.address, body.firebase_id, body.status, body.comment], (err, res) =>{
         if (err) {
           console.log("Unexpected database error: " + err);
           resolve(null);
